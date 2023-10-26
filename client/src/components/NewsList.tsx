a0296576d5be500862afa11d4f469f740850748b
take-cantik <take.cantik17@gmail.com>
2023-10-26 19:48:07 +0900

    :sparkles: Add NewsList component

---
+++ client/src/components/NewsList.tsx
@@ -0,0 +1,18 @@
+import { News } from "../types";
+
+type NewsListProps = {
+  newsList: News[];
+};
+
+export const NewsList = ({ newsList }: NewsListProps) => (
+  <ul className="w-full">
+    {newsList.map((news) => (
+      <li className="w-full pt-4 after:content-[''] after:w-full after:h-[1px] after:block after:mt-3 after:bg-black-lighten-2">
+        <a
+          href={news.path}
+          className="w-full block px-3 text-base text-black-lighten-1 hover:text-black overflow-hidden text-ellipsis whitespace-nowrap duration-300"
+        >{`${news.date}: ${news.content}`}</a>
+      </li>
+    ))}
+  </ul>
+);
