be9a6c21b9065434a82bbb8d589d387140080a0d
take-cantik <take.cantik17@gmail.com>
2023-10-26 20:05:17 +0900

    :adhesive_bandage: Fix styles

--- client/src/components/NewsList.tsx
+++ client/src/components/NewsList.tsx
@@ -7,7 +7,10 @@ type NewsListProps = {
 export const NewsList = ({ newsList }: NewsListProps) => (
   <ul className="w-full">
     {newsList.map((news) => (
-      <li className="w-full pt-4 after:content-[''] after:w-full after:h-[1px] after:block after:mt-3 after:bg-black-lighten-2">
+      <li
+        key={news.path}
+        className="w-full pt-4 after:content-[''] after:w-full after:h-[1px] after:block after:mt-3 after:bg-black-lighten-2"
+      >
         <a
           href={news.path}
           className="w-full block px-3 text-base text-black-lighten-1 hover:text-black overflow-hidden text-ellipsis whitespace-nowrap duration-300"
