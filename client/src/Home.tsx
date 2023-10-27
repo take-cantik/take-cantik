58126abd72d3b5ecabad406ff14dd28c3442bb06
take-cantik <take.cantik17@gmail.com>
2023-10-27 12:40:48 +0900

    :sparkles: Add Home page

---
+++ client/src/Home.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./components/Container";
+import { Header } from "./components/Header";
+import { NewsList } from "./components/NewsList";
+import { SocialLink } from "./components/SocialCard";
+import { bio, newsList, socialList } from "./data/home";
+
+function Home() {
+  return (
+    <>
+      <Header>take-cantik.dev</Header>
+      <main className="w-full min-h-screen bg-gray-lighten-2">
+        <Container>
+          <section className="w-full">
+            <h2 className="text-lg mb-4">take-cantik</h2>
+            <p className="w-full px-4 mt-3 whitespace-pre-wrap">{bio}</p>
+          </section>
+          <section className="w-full mt-5">
+            <h2 className="text-lg mb-4">Social</h2>
+            <nav className="w-full grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] gap-5 justify-items-center px-4">
+              {socialList.map((social) => (
+                <SocialLink social={social} />
+              ))}
+            </nav>
+          </section>
+          <section className="w-full mt-5">
+            <h2>News</h2>
+            <NewsList newsList={newsList} />
+          </section>
+        </Container>
+      </main>
+    </>
+  );
+}
+
+export default Home;
