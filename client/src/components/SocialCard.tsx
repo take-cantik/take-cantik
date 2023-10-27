76726df5318b918bb58707bd7fcf59407da254dd
take-cantik <take.cantik17@gmail.com>
2023-10-27 12:40:05 +0900

    :zap: Update style

--- client/src/components/SocialCard.tsx
+++ client/src/components/SocialCard.tsx
@@ -8,12 +8,12 @@ export const SocialLink = ({ social }: SocialCardProps) => (
   <a
     href={social.profileUrl}
     target="_blank"
-    className="w-full max-w-[120px] min-w-[100px] flex flex-col items-center py-3 gap-2 rounded-xl bg-white text-xs text-black-lighten-1 duration-300 hover:scale-105"
+    className="w-full max-w-[140px] min-w-[100px] flex flex-col items-center pt-5 pb-3 gap-2 rounded-xl bg-white text-xs text-black-lighten-1 duration-300 hover:scale-105"
   >
     <img
       src={social.iconPath}
       alt={social.name}
-      className="w-[48px] h-[48px] rounded-lg"
+      className="w-[40px] h-[40px]"
     />
     {social.domain}
   </a>
