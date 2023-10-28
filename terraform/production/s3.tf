630d67155d58b35ff79249c2302cf0d2ab2415e2
take-cantik <take.cantik17@gmail.com>
2023-10-28 14:27:27 +0900

    :sparkles: Add S3

---
+++ terraform/production/s3.tf
@@ -0,0 +1,47 @@
+# ---------------------------
+# S3
+# ---------------------------
+
+# バケットの作成
+resource "aws_s3_bucket" "hosting_s3" {
+  bucket = "${var.project_name}-${var.env}-hosting-s3"
+
+  tags = {
+    Name = "${var.project_name}-${var.env}-hosting-s3"
+  }
+}
+
+# 静的ホスティングの設定
+resource "aws_s3_bucket_website_configuration" "hosting_config" {
+  bucket = aws_s3_bucket.hosting_s3.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# バケットポリシー
+resource "aws_s3_bucket_policy" "hosting_policy" {
+  bucket = aws_s3_bucket.hosting_s3.id
+
+  # Cloudfrontからのみアクセスを許可
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Id        = "PolicyForCloudFrontPrivateContent",
+    Statement = [
+      {
+        Sid       = "CloudFrontAccess",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action    = "s3:GetObject",
+        Resource = "${aws_s3_bucket.hosting_s3.arn}/*",
+      },
+    ]
+  })
+}
