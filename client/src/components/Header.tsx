80e31e264f6602a782d9358a12ce47420e8d30f1
take-cantik <take.cantik17@gmail.com>
2023-10-28 20:04:47 +0900

    :zap: Update Header str style

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps } from "react";
 
 export const Header = ({ children }: ComponentProps<"header">) => (
-  <header className="w-full flex items-center gap-4 px-4 lg:px-8 py-3 bg-gray-lighten-1">
+  <header className="w-full flex items-center gap-4 px-4 lg:px-8 py-3 text-lg lg:text-xl bg-gray-lighten-1">
     <img
       src="/images/take-cantik.jpg"
       alt="take-cantik's icon"
