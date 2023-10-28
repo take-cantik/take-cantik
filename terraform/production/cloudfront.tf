df587437cc25c7b6184a5255a9bf6e05766caccb
take-cantik <take.cantik17@gmail.com>
2023-10-28 18:23:44 +0900

    :zap: Add custom_error_response

--- terraform/production/cloudfront.tf
+++ terraform/production/cloudfront.tf
@@ -27,6 +27,13 @@ resource "aws_cloudfront_distribution" "web_cf" {
     viewer_protocol_policy   = "redirect-to-https"
   }
 
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 403
+    response_code         = 403
+    response_page_path    = "/"
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
