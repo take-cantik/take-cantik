0044bf11e7784a15f5b74b01bddbb70d4dce2e3c
take-cantik <take.cantik17@gmail.com>
2023-10-28 18:13:52 +0900

    :zap: Update error page style

--- client/src/pages/error/index.tsx
+++ client/src/pages/error/index.tsx
@@ -6,10 +6,16 @@ type ErrorProps = {
 };
 
 const Error = ({ code, message }: ErrorProps) => (
-  <main>
-    <h2>{code}</h2>
-    <p>{message}</p>
-    <Link to="/">ホームへ戻る</Link>
+  <main className="w-full min-h-screen flex items-center justify-center px-4 bg-gray-lighten-2">
+    <div className="w-full max-w-[540px] flex flex-col items-center justify-center py-10 px-4 rounded-lg bg-white">
+      <h2 className="text-5xl lg:text-6xl font-extrabold mb-2 lg:mb-4">
+        {code}
+      </h2>
+      <p className="text-lg lg:text-xl mb-6 lg:mb-8">{message}</p>
+      <Link to="/" className=" border-b-2 border-solid">
+        ホームへ戻る
+      </Link>
+    </div>
   </main>
 );
 
