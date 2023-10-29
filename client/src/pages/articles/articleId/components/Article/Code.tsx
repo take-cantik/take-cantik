701eefd1d2cac2d654aa268c7ce18f79bc7501e3
take-cantik <take.cantik17@gmail.com>
2023-10-29 14:44:40 +0900

    :sparkles: Add custome code component

---
+++ client/src/pages/articles/articleId/components/Article/Code.tsx
@@ -0,0 +1,22 @@
+import { ClassAttributes, HTMLAttributes } from "react";
+import { ExtraProps } from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { github } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+type CodeProps = ClassAttributes<HTMLElement> &
+  HTMLAttributes<HTMLElement> &
+  ExtraProps;
+
+export const Code = ({ children, className, ...props }: CodeProps) => {
+  const match = /language-(\w+)/.exec(className || "");
+
+  return match ? (
+    <SyntaxHighlighter language={match[1]} styles={github}>
+      {String(children).replace(/\n$/, "")}
+    </SyntaxHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
+  );
+};
