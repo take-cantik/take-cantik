c27e2ed0e42bc65c964a8972f672dbefebb8f44f
take-cantik <take.cantik17@gmail.com>
2023-11-04 19:07:04 +0900

    :zap: Set object-contain to social image

--- client/src/pages/home/components/SocialCard.tsx
+++ client/src/pages/home/components/SocialCard.tsx
@@ -11,7 +11,13 @@ export const SocialLink = ({ social }: SocialCardProps) => (
     target="_blank"
     className="w-full max-w-[140px] min-w-[100px] flex flex-col items-center pt-5 pb-3 gap-2 rounded-xl bg-white text-xs text-black-lighten-1 duration-300 hover:scale-105"
   >
-    <Image src={social.iconPath} alt={social.name} width={40} height={40} />
+    <Image
+      src={social.iconPath}
+      alt={social.name}
+      width={40}
+      height={40}
+      className="w-10 h-10 object-contain"
+    />
     {social.domain}
   </a>
 );
