add96be8276b26c2d2d3441b106abe5cd829be4c
take-cantik <take.cantik17@gmail.com>
2023-11-04 16:45:31 +0900

    :sparkles: Add home components

---
+++ client/src/pages/home/types/index.ts
@@ -0,0 +1,12 @@
+export type Social = {
+  name: string;
+  iconPath: string;
+  domain: string;
+  profileUrl: string;
+};
+
+export type News = {
+  date: string;
+  content: string;
+  path?: string;
+};
