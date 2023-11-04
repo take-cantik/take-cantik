2f52ae549d104cdab2ba083d7ada5cd426faa065
take-cantik <take.cantik17@gmail.com>
2023-11-04 17:05:49 +0900

    :zap: Set Head

--- client/src/pages/_app.page.tsx
+++ client/src/pages/_app.page.tsx
@@ -1,7 +1,25 @@
 import "~/styles/reset.css";
 import "~/styles/global.css";
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        title="take-cantik.com"
+        description="take-cantikの個人ページ"
+        canonical="https://take-cantik.com/"
+        openGraph={{
+          site_name: "take-cantik.com",
+          title: "take-cantik",
+          type: "website",
+          url: "https://take-cantik.com/",
+          description: "take-cantikの個人ページ",
+          images: [{ url: "https://take-cantik.com/images/take-cantik.jpg" }],
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
