7552f99eeb98cad881d6b5de3d8517ceb9f791a6
take-cantik <take.cantik17@gmail.com>
2023-11-05 02:13:52 +0900

    :fire: Delete react files

--- react-client/src/components/Container.tsx
+++
@@ -1,5 +0,0 @@
-import { ComponentProps } from "react";
-
-export const Container = ({ children }: ComponentProps<"div">) => (
-  <div className="w-full max-w-5xl py-10 px-4 lg:px-8 mx-auto">{children}</div>
-);
