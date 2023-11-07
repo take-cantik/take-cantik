e5d9455aae749451c1aec8acb06e0ddab7ae37ce
take-cantik <take.cantik17@gmail.com>
2023-11-08 00:57:10 +0900

    :zap: Add noreferrer to anchor tag

--- client/src/pages/home/components/SocialCard.tsx
+++ client/src/pages/home/components/SocialCard.tsx
@@ -9,6 +9,7 @@ export const SocialLink = ({ social }: SocialCardProps) => (
   <a
     href={social.profileUrl}
     target="_blank"
+    rel="noreferrer"
     className="w-full max-w-[140px] min-w-[100px] flex flex-col items-center pt-5 pb-3 gap-2 rounded-xl bg-white text-xs text-black-lighten-1 duration-300 hover:scale-105"
   >
     <Image
