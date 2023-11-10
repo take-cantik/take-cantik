ba4682b267218b712ab7a1abaed2b93cbd23acc8
take-cantik <take.cantik17@gmail.com>
2023-11-10 19:03:38 +0900

    :bug: コードブロックがinlineの時の処理を追加

--- client/src/pages/articles/[articleId]/components/Article/Code.tsx
+++ client/src/pages/articles/[articleId]/components/Article/Code.tsx
@@ -1,16 +1,22 @@
 import { ClassAttributes, HTMLAttributes } from "react";
 import { ExtraProps } from "react-markdown";
-import SyntaxHighlighter from "react-syntax-highlighter";
+import SyntaxHighlighter, {
+  SyntaxHighlighterProps,
+} from "react-syntax-highlighter";
 import { github } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
-type CodeProps = ClassAttributes<HTMLElement> &
-  HTMLAttributes<HTMLElement> &
-  ExtraProps;
+type CodeProps = Omit<
+  SyntaxHighlighterProps &
+    ClassAttributes<HTMLElement> &
+    HTMLAttributes<HTMLElement> &
+    ExtraProps,
+  "styles"
+>;
 
-export const Code = ({ children, className }: CodeProps) => {
+export const Code = ({ inline, children, className }: CodeProps) => {
   const match = /language-(\w+)/.exec(className || "");
 
-  return (
+  return inline ? (
     <SyntaxHighlighter
       language={match ? match[1] : "txt"}
       styles={github}
@@ -18,5 +24,7 @@ export const Code = ({ children, className }: CodeProps) => {
     >
       {String(children).replace(/\n$/, "")}
     </SyntaxHighlighter>
+  ) : (
+    <code className="py-1 px-1 rounded bg-gray-lighten-1">{children}</code>
   );
 };
